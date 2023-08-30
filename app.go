9443bbbeb01362a5580e3231f80b4c1e1cebd69b
jutionck <jutionck@gmail.com>
2023-08-30 20:03:32 +0700

    :lock: Add JWT

--- app.go
+++ app.go
@@ -1,59 +1,135 @@
 package main
 
-import "github.com/jutionck/golang-upskilling-agt/delivery"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
 
-// var log = logrus.New()
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/jutionck/golang-upskilling-agt/model"
+)
 
-// otomatis terbaca
-// func init() {
-// 	log.SetFormatter(&logrus.JSONFormatter{})
-// 	log.SetLevel(logrus.DebugLevel)
-// }
+var ApplicationName = "USPKILLINGGO"
+var JwtSigningMethod = jwt.SigningMethodHS256
+var JwtSignatureKey = []byte("JozzG4nD0ZZ!!!")
 
-func main() {
-	delivery.NewServer().Run()
-
-	// buat file
-	// file, err := os.OpenFile("logger.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	// if err != nil {
-	// 	log.Info("Failed to log to file!")
-	// } else {
-	// 	log.Out = file
-	// 	defer file.Close()
-	// }
-
-	//
-	// log.WithFields(logrus.Fields{
-	// 	"project": "Golang Upskilling",
-	// }).Info("Error message")
-
-	// log.WithFields(logrus.Fields{
-	// 	"project": "Golang Upskilling",
-	// }).Warn("Warning message")
-
-	// // fake error
-	// err = errors.New("this is error message")
-	// if err != nil {
-	// 	log.WithFields(logrus.Fields{
-	// 		"project": "Golang Upskilling",
-	// 	}).Error("Error message")
-	// }
+type MyClaim struct {
+	jwt.RegisteredClaims
+	Username string `json:"username"`
+	Role     string `json:"role"`
 }
 
-// Native (package diluar built-in)
-// 1. Library Driver Database => Postgres => lib/pq atau bisa menggunakan pgx
-// 2. Library Godotenv atau Viper => Environment Configuration
-// 3. ID => md5 atau hash 2893927-8u38e3nd0-dndd3 => google/uuid
-// 4. Password Hashing => bcrypt
+type authHeader struct {
+	AuthorizationHeader string `header:"Authorization"`
+}
 
-// SQLC
-// 1. Library Driver Database => Postgres => lib/pq atau bisa menggunakan pgx
-// 2. Library Godotenv atau Viper => Environment Configuration
-// 3. ID => md5 atau hash 2893927-8u38e3nd0-dndd3 => google/uuid
-// 4. Password Hashing => bcrypt
+// middleware
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.URL.Path == "/api/v1/auth/login" {
+			c.Next()
+		} else {
+			var aH authHeader
+			if err := c.ShouldBindHeader(&aH); err != nil {
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
+				return
+			}
 
-// GORM
-// 1. Driver Database => GORM
-// 2. Library Godotenv atau Viper => Environment Configuration
-// 3. ID => md5 atau hash 2893927-8u38e3nd0-dndd3 => google/uuid
-// 4. Password Hashing => bcrypt
+			// kita akan dapatkan tokennya dari Authorization Header
+			// Replace => Bearer token menjadi token ==== menggunakan strings.Replace
+			tokenString := strings.Replace(aH.AuthorizationHeader, "Bearer ", "", -1)
+			if tokenString == "" {
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
+				return
+			}
+
+			// Verifikasi dulu apakah token nya valid atau tidak
+			claims, err := VerifyAccessToken(tokenString)
+			if err != nil {
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Unauthorized"})
+				return
+			}
+
+			c.Set("claims", claims)
+			fmt.Println("claims:", claims)
+
+			c.Next()
+		}
+	}
+}
+
+func CreateAccessToken(user model.User) (string, error) {
+	now := time.Now().UTC()
+	end := now.Add(5 * time.Minute)
+	claims := MyClaim{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    ApplicationName,
+			IssuedAt:  jwt.NewNumericDate(now),
+			ExpiresAt: jwt.NewNumericDate(end),
+		},
+		Username: user.Username,
+		Role:     user.Role,
+	}
+
+	jwtNewClaim := jwt.NewWithClaims(JwtSigningMethod, claims)
+	token, err := jwtNewClaim.SignedString(JwtSignatureKey)
+	if err != nil {
+		return "", errors.New("failed to create access token")
+	}
+
+	return token, nil
+}
+
+func VerifyAccessToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if method, ok := t.Method.(*jwt.SigningMethodHMAC); !ok || method != JwtSigningMethod {
+			return nil, errors.New("signing method invalid")
+		}
+		return JwtSignatureKey, nil
+	})
+
+	if err != nil {
+		return nil, errors.New("failed to verify access token")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid || claims["iss"] != ApplicationName {
+		return nil, errors.New("invalid token claims")
+	}
+
+	return claims, nil
+}
+
+func main() {
+	// delivery.NewServer().Run()
+
+	r := gin.Default()
+	rg := r.Group("/api/v1")
+	rg.Use(AuthMiddleware())
+	rg.POST("/auth/login", func(c *gin.Context) {
+		var userCredential model.User
+		if err := c.ShouldBindJSON(&userCredential); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+
+		if userCredential.Username == "admin" && userCredential.Password == "password" {
+			userCredential.Role = "admin"
+			token, err := CreateAccessToken(userCredential)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+				return
+			}
+			c.JSON(http.StatusCreated, gin.H{"data": token})
+		}
+	})
+
+	rg.GET("/users", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Ok"})
+	})
+
+	r.Run(":8888")
+}
